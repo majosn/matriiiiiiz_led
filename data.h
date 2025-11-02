1e90e69c6ff3e4f0d4a13d588080650031de9430
chemsBurrow <a01739515@tec.mx>
2025-11-02 14:24:39 -0600

    Create data.h

---
+++ data.h
@@ -0,0 +1,7 @@
+// RED DE INTERNET
+const char* ssid_1 = "Mega_2.4G_3868"; 
+const char* password_1 = "TtsfCC5e";
+
+// DATA BROKER
+const char* BrokerMQTT = "broker.emqx.io"; 
+const char* NombreESP = "";
